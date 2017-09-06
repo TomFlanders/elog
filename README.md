e4b769729155c92b3483060de3f9bf0bf6c939c4
Tom Flanders <tom@tomflanders.com>
2017-09-05 19:18:13 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# elog
+Show contents of error_log
+
+## Installation
+1. Download files to WordPressRoot/wp-content/plugins/elog.
+2. Activate plugin through admin screen.
+3. Add the [elog] shortcode where you want to display the info.
+
+## License
+GNU GENERAL PUBLIC LICENSE Version 3
+
+## Author
+tom@tomflanders.com
---
+++ elog.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Plugin Name: elog
+Plugin URI:  http://tomflanders.com/development/
+Description: error_log reader
+Version:     0.5
+Author:      Tom Flanders
+Author URI:  http://tomflanders.com
+License:     GPL3
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+/** Create menu */
+add_action( 'admin_menu', 'elog_menu' );
+
+/** Create page */
+function elog_menu() {
+	add_menu_page( "elog", "elog", "manage_options", "elog display", 'my_plugin_options');
+}
+
+/** Add content to page */
+function my_plugin_options() {
+	$elog_root = $_SERVER['DOCUMENT_ROOT'];
+	$elog_count = 0;
+	foreach (glob("../error_log") as $elog_filename) {
+		echo "<br/>$elog_filename<br/>\n";
+		$elog_count++;
+		echo "<pre>" . file_get_contents($elog_filename) . "</pre>";
+
+	}
+	foreach (glob("../wp-content/*/*/error_log") as $elog_filename) {
+		echo "<br/>$elog_filename<br/>\n";
+		$elog_count++;
+		echo "<pre>" . file_get_contents($elog_filename) . "</pre>";
+
+	}
+	echo "<p>Logs found: " . $elog_count . "</p>";
+}
+
+?>
