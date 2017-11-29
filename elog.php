2671950b00b45c4a66f1b02d781c76a2601e00b2
Tom Flanders <tom@tomflanders.com>
2017-11-29 15:08:41 -0800

    removing image folder

--- elog.php
+++ elog.php
@@ -3,11 +3,7 @@
 Plugin Name: elog
 Plugin URI:  https://github.com/TomFlanders/elog
 Description: error_log reader
-<<<<<<< HEAD
 Version:     1.0.0
-=======
-Version:     0.8.2
->>>>>>> d1c289290010e46829503c2135e47e8a95dae359
 Author:      Tom Flanders
 Author URI:  http://tomflanders.com
 License:     GPL3
