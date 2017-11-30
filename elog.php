2c3a849350020468cc7ade3a729d6753d35959b4
Tom Flanders <tom@tomflanders.com>
2017-11-30 12:53:59 -0800

    fixed unsafe paths

--- elog.php
+++ elog.php
@@ -11,28 +11,29 @@ License URI: https://www.gnu.org/licenses/gpl-3.0.html
 */
 
 /** Create menu */
-add_action( 'admin_menu', 'elog_menu' );
+add_action( 'admin_menu', 'tpfelog_menu' );
 
 /** Create page */
-function elog_menu() {
-	add_menu_page( "elog", "elog", "manage_options", "elog display", 'elog_options');
+function tpfelog_menu() {
+	add_menu_page( "tpfelog", "elog", "manage_options", "elog display", 'tpfelog_options');
 }
 
 /** Add content to page */
-function elog_options() {
-	$elog_root = $_SERVER['DOCUMENT_ROOT'];
-	$elog_count = 0;
-	$elog_folders = array("../error_log", "../wp-*/error_log", "../wp-*/*/error_log", "../wp-*/*/*/error_log","../php_errorlog", "../wp-*/php_errorlog", "../wp-*/*/php_errorlog", "../wp-*/*/*/php_errorlog");
-	foreach($elog_folders as $elog_folder){
-		foreach (glob($elog_folder) as $elog_filename) {
-			if (strpos($elog_filename, 'backup') != true) {
-			echo "<br/>$elog_filename<br/>";
-			$elog_count++;
-			echo "<pre>" . file_get_contents($elog_filename) . "</pre>";
+function tpfelog_options() {
+	$tpfelog_root = $_SERVER['DOCUMENT_ROOT'];
+	$tpfelog_count = 0;
+	$tpfelog_path = ABSPATH;
+	$tpfelog_folders = array($tpfelog_path . "error_log", $tpfelog_path . "wp-*/error_log", $tpfelog_path . "wp-*/*/error_log", $tpfelog_path . "wp-*/*/*/error_log",$tpfelog_path . "php_errorlog", $tpfelog_path . "wp-*/php_errorlog", $tpfelog_path . "wp-*/*/php_errorlog", $tpfelog_path . "wp-*/*/*/php_errorlog");
+	foreach($tpfelog_folders as $tpfelog_folder){
+		foreach (glob($tpfelog_folder) as $tpfelog_filename) {
+			if (strpos($tpfelog_filename, 'backup') != true) {
+			echo "<br/>$tpfelog_filename<br/>";
+			$tpfelog_count++;
+			echo "<pre>" . file_get_contents($tpfelog_filename) . "</pre>";
 			}
 		}
 	}
-	echo "<p>Logs found: " . $elog_count . "</p>";
+	echo "<p>Logs found: " . $tpfelog_count . "</p>";
 }
 
 ?>
